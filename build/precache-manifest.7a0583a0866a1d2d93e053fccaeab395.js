9e22b1f2bafc4fa90c0a618967ee7561de309f16
Andy <924115911@qq.com>
2019-01-24 11:46:59 +0800

    add build

---
+++ build/precache-manifest.7a0583a0866a1d2d93e053fccaeab395.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11fbfb9f4c0987053a70",
+    "url": "./static/css/main.2c1360ef.chunk.css"
+  },
+  {
+    "revision": "11fbfb9f4c0987053a70",
+    "url": "./static/js/main.11fbfb9f.chunk.js"
+  },
+  {
+    "revision": "14317e414f53dccf0592",
+    "url": "./static/js/1.14317e41.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "436bf76b25679c2c84784f90d18a068a",
+    "url": "./index.html"
+  }
+];
